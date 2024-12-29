51ab98a132130b305ed32d8f04680d0ede89160e
Salim <salimofshadow@outlook.it>
2024-12-30 00:34:50 +0100

    Unit => String

--- src/main/scala/utils/ImageUploader.scala
+++ src/main/scala/utils/ImageUploader.scala
@@ -3,13 +3,17 @@ package utils
 import scalafx.scene.image.Image
 
 case class ImageUploader (image: Image,uploadingSite: String) {
-  def retrieveEndpoint(): Unit = {
-    uploadingSite match{
-      case "imgbb" =>  println("imgbb")  // Actual endpoints go here
-      case "imgur" =>  println("imgur")
-      case _ =>  println("Invalid Url Domain")
+  def retrieveEndpoint(): String = {
+    uploadingSite match {
+      case "imgbb" => "imgbb" // Actual endpoints go here
+      case "imgur" => "imgur"
+      case _ => "Invalid Url Domain"
     }
   }
 
-  def uploadPicture() = ???
+  def uploadPicture(): Unit = {
+    val endpoint: String = retrieveEndpoint()
+
+
+  }
 }
